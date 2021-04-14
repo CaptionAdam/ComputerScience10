44f27eb9902b8ec00c7c3f18e44dee8462485b64
CaptionAdam <adamkuefler8@gmail.com>
2021-04-13 20:09:15 -0600

    Update RecSize.py

--- Python/RecSize.py
+++ Python/RecSize.py
@@ -1,7 +1,7 @@
 #Python rectangle size finder
-print("Hello there how wide is your Rectange? \n(In cm)")
+print("Hello there how wide is your Rectange? \n(In mm)")
 lenght=input()
-print("Ok, How tall is the Rectangle? \n(In cm)")
+print("Ok, How tall is the Rectangle? \n(In mm)")
 hight=input()
 print("One Moment Please \nPRESS ENTER")
 input()
@@ -9,7 +9,7 @@ area = int(lenght) * int(hight)
 perimeter = int(lenght) + int(hight) * 2
 #int(parimiter) * 2
 print("Your Rectangle")
-print(f"Has a width of {lenght}(cm) wide")
-print(f"Has a hight of {hight}(cm)tall")
-print(f"Has an area = {area}(cm^2)")
-print(f"has a perimeter = {perimeter}(cm)")
+print(f"Has a width of {lenght}(mm) wide")
+print(f"Has a hight of {hight}(mm)tall")
+print(f"Has an area = {area}(mm^2)")
+print(f"has a perimeter = {perimeter}(mm)")
